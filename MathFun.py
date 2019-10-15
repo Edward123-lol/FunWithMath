7a8689733b62cd5eeff88e2bb1df00c50542b98a
100128112 <edward05.jang@gmail.com>
2019-10-15 15:26:19 -0500

    Math things

--- MathFun.py
+++ MathFun.py
@@ -25,3 +25,16 @@ print(archimedes(16))
 
 for sides in range(8, 100, 8):
     print(sides, archimedes(sides))
+
+# See the loop above. In addition to the value of pi, print the difference
+# between the values calculated by the archimedes function and by math.pi.
+# How many sides does it to make the two close.
+
+
+# Accumulators
+
+acc = 0
+for val in range(1, 6):
+    acc = acc + val
+
+print(acc)
